71fc40b690aad175a31eaee1130e4faffcf74a2e
Irrisam <tristanduchamp@hotmail.fr>
2023-05-30 10:08:39 +0200

    MY BAD

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,6 @@
 import random
 number = random.randint(-10000, 10000)
 
-number = -123
-
 if number < 0:
     numberf = - number
 else:
@@ -11,11 +9,13 @@ else:
 
 comparer = numberf % 10
 
+negdigit = comparer = - comparer
+
 if comparer == 0:
     print(f"Last digit of {number} is {comparer} and is 0")
 
 elif comparer < 6 and comparer != 0:
-    print(f"Last digit of {number} is -{comparer} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {negdigit} and is less than 6 and not 0")
 
 else:
     print(f"Last digit of {number} is {comparer} and is greater than 5")

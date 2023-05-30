25f781e9f77b1fe5c71f315313b0263c4595f611
Irrisam <tristanduchamp@hotmail.fr>
2023-05-30 09:49:41 +0200

    trying 1 print function

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i == 99:
-        print(f"{i:02}\n", end="")
-    else:
-        print(f"{i:02}, ", end="")
+for i in range(100):
+    print(f"{i:02}" + (", " if i != 99 else "\n"), end="")

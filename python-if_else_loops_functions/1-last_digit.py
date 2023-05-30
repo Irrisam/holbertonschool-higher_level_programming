05715fe6df72c5d32cc20883e68c42d36d52bd1f
Irrisam <tristanduchamp@hotmail.fr>
2023-05-30 10:04:42 +0200

    fix our great checker

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 
+number = -123
+
 if number < 0:
     numberf = - number
 else:
@@ -13,7 +15,7 @@ if comparer == 0:
     print(f"Last digit of {number} is {comparer} and is 0")
 
 elif comparer < 6 and comparer != 0:
-    print(f"Last digit of {number} is {comparer} and is less than 6 and not 0")
+    print(f"Last digit of {number} is -{comparer} and is less than 6 and not 0")
 
 else:
     print(f"Last digit of {number} is {comparer} and is greater than 5")

fd3cb4cdd88eae15d2fe371a6fd3011a0c6c28e4
Irrisam <tristanduchamp@hotmail.fr>
2023-06-05 15:02:00 +0200

    trying to correct doscstring still

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,11 +4,11 @@ def element_at(my_list, idx):
     Print index.
 
     Args:
-        my_list (_type_): List of element to print from
-        idx (_type_): Index of element to print
+        my_list (list): List of element to print from
+        idx (index): Index of element to print
 
     Returns:
-        _type_: None on error
+       None on error
     """
     if idx < 0 or idx > len(my_list):
         return (None)

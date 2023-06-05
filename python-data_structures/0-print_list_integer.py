0fe657f05edde2268309bc9d55646d18e79b43b0
Irrisam <tristanduchamp@hotmail.fr>
2023-06-05 14:41:16 +0200

    tried to add doc to file

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """
+    Print the integers in a list.
+
+    Args:
+        my_list (list): The list of integers to be printed.
+
+    Returns:
+        None
+    """
     for i in my_list:
         print("{:d}".format(i))
-
-
-print_list_integer([1, 2, 3, 4, 5])

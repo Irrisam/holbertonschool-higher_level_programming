61f6e21c697a4d51fd72f2820bf03eb33d79512b
Irrisam <tristanduchamp@hotmail.fr>
2023-06-05 14:50:33 +0200

    first push of 2nd task

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """
+
+    Args:
+        my_list (_type_): List of element to print from
+        idx (_type_): Index of element to print
+
+    Returns:
+        _type_: None on error
+    """
+    if idx < 0 or idx > len(my_list):
+        return (None)
+    print("Element at index {:d} is {:d}".format(idx, len(my_list)))

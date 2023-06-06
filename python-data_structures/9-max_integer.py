629bca7958d4368f2b77feae598aa9f31afdd95f
Irrisam <tristanduchamp@hotmail.fr>
2023-06-06 15:53:49 +0200

    fixed neg values

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    result = 0
     if my_list is None or len(my_list) == 0:
         return None
+    result = my_list[0]
     for i in my_list:
         if i > result:
             result = i
     return result
+
+
+my_list = [-5, -90, -2, -13, -34, -5, -13, -3]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))

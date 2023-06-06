bc63806853f46859849d1be895905c1f7f8172cf
Irrisam <tristanduchamp@hotmail.fr>
2023-06-06 12:17:43 +0200

    corrected format

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
-    for i in my_list[0:]:
-        print("{}", i)
+    for i in my_list:
+        print("{}".format(i))
+
+
+print_reversed_list_integer([1, 2, 3, 4, 5])

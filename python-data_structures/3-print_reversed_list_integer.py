0f4d3443f94a6230fe2d5ace588a6a03a7f943b5
Irrisam <tristanduchamp@hotmail.fr>
2023-06-06 12:19:26 +0200

    checked if parameter is null

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
+    if (my_list is None):
+        my_list.reverse()
     for i in my_list:
         print("{}".format(i))
-
-
-print_reversed_list_integer([1, 2, 3, 4, 5])

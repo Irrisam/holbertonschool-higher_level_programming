5ab41112c6c2af910da4c904bc31b1149a87920b
Irrisam <tristanduchamp@hotmail.fr>
2023-06-06 15:26:37 +0200

    corrected empty file return

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return (len(sentence), sentence[0])
+    if sentence is None:
+        return (len(sentence), None)
+    else:
+        return (len(sentence), sentence[0])

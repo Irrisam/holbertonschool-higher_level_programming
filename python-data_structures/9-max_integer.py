9bcc734bfaa1fd5ab7ec8f8cbce5cdd3d23bd6d6
Irrisam <tristanduchamp@hotmail.fr>
2023-06-06 15:54:33 +0200

    removed test code

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -7,8 +7,3 @@ def max_integer(my_list=[]):
         if i > result:
             result = i
     return result
-
-
-my_list = [-5, -90, -2, -13, -34, -5, -13, -3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))

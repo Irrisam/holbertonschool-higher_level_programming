d5ca114e367cab43edebcc143f319472c77f6767
Irrisam <tristanduchamp@hotmail.fr>
2023-06-06 13:37:52 +0200

    why tho

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,15 @@ def print_matrix_integer(matrix=[[]]):
         return None
     for row in matrix:
         for element in row:
-            print("{:d}".format(element), end=' ')
+            print("{}".format(element), end=' ')
         print()
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()

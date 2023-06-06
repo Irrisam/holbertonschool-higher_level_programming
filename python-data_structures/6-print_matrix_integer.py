68963484fcf9f920fd3921983237918a703ddf6a
Irrisam <tristanduchamp@hotmail.fr>
2023-06-06 13:49:29 +0200

    refactored characters

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return None
     for row in matrix:
+        sep = ""
         for element in row:
-            print("{}".format(element), end=' ')
+            print("{}{:d}".format(sep, element), end='')
+            sep = ", "
         print()
 
 

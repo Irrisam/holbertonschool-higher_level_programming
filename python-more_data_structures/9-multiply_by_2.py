abc4348cc7f73f1f0c015749ec74d87915e61163
Irrisam <tristanduchamp@hotmail.fr>
2023-06-08 13:49:33 +0200

    correct task 09

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,7 @@
 def multiply_by_2(a_dictionary):
     if a_dictionary is None:
         return None
-    result = 0
+    new_dict = {}
     for i in a_dictionary:
-        a_dictionary[i] = a_dictionary[i] * 2
-    return result
+        new_dict[i] = a_dictionary[i] * 2
+    return new_dict

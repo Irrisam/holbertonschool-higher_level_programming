9795db7c1bddc9caaf1f53b1ecb27fa7141dc212
Irrisam <tristanduchamp@hotmail.fr>
2023-06-08 14:43:49 +0200

    try to correct requirements

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    if my_list is None or len(my_list) == 0:
-        return None
     result = list(map(lambda x: x * number, my_list))
     return result

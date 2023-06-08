3b6dd1ec83a3d449fd933788cbae4ca6fef8758a
Irrisam <tristanduchamp@hotmail.fr>
2023-06-08 10:54:30 +0200

    corrected second task

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,9 +2,10 @@
 def search_replace(my_list, search, replace):
     if my_list is None or my_list == 0:
         return None
-    counter = 0
+    new_list = []
     for i in my_list:
         if i == search:
-            my_list[counter] = replace
-        counter += 1
-    return my_list
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return new_list

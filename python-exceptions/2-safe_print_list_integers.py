07cc2510fdb52a4fe0ba0397e27fce49b9df5d7f
Irrisam <tristanduchamp@hotmail.fr>
2023-06-09 06:55:54 +0200

    refactor to reduce variables

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    if my_list is None:
-        return None
-    a = 0
-    for j in range(x):
+
+    count = 0
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[j]), end="")
-            a += 1
-        except (ValueError, TypeError):
-            j += 1
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (TypeError, ValueError):
+            pass
     print()
-    return a
-
-
-my_list = [1, 2, 3, 4, 5]
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-
+    return count

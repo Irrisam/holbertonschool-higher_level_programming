ea2fea5b2d22e84dcf815d1f89a1a85d8296d038
Irrisam <tristanduchamp@hotmail.fr>
2023-06-09 06:32:28 +0200

    correct print number + error handling

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@ def safe_print_list(my_list=[], x=0):
     if my_list is None or my_list == 0:
         return None
     cnt = 0
-    for _ in my_list:
-        cnt += 1
     try:
-        print("{}".format(my_list))
-    except TypeError:
-        pass
-    return cnt
+        for j in range(x): 
+            print("{}".format(my_list[j]), end="")
+    except IndexError:
+        x = j
+    print()
+    return x

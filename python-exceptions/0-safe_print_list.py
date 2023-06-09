f77df5a36dcffe86166ac15b37b294969523cfe1
Irrisam <tristanduchamp@hotmail.fr>
2023-06-09 06:34:06 +0200

    remove hidden trailling whitespace..

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         return None
     cnt = 0
     try:
-        for j in range(x): 
+        for j in range(x):
             print("{}".format(my_list[j]), end="")
     except IndexError:
         x = j

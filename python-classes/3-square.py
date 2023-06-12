219723177848dceecc8d3cc64ab84f30b58e8096
Irrisam <tristanduchamp@hotmail.fr>
2023-06-12 10:56:27 +0200

    add task 04

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A module with as simple empty Square class"""
+
+
+class Square:
+    """Class defining a square."""
+
+    def __init__(self, size=0):
+        """Defines square."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Method returning square area
+
+        Returns:
+            Integer: Return value of square area of square
+        """
+        return self.__size ** 2

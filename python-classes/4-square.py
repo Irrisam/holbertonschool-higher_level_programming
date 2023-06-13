aca73f8f468157bfa60b8b48dd10febe2f28a4fa
Irrisam <tristanduchamp@hotmail.fr>
2023-06-13 09:51:47 +0200

    remove main

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -30,16 +30,3 @@ class Square:
             Integer: Return value of square area of square
         """
         return self.__size ** 2
-
-
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)

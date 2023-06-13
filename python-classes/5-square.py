7cc106546fd631581b0988133754d2b13c903ed0
Irrisam <tristanduchamp@hotmail.fr>
2023-06-13 10:22:39 +0200

    add task 06

---
+++ python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""A module with as simple empty Square class"""
+
+
+class Square:
+    """Class defining a square."""
+
+    def __init__(self, size=0):
+        """Defines square."""
+        self.__size = 0
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Method returning square area
+
+        Returns:
+            Integer: Return value of square area of square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Printing # for the squares
+        """
+        for i in range(self.size):
+            if self.size != 0:
+                print("#" * self.size)
+            else:
+                print("", end="")

bdcaae7783b7a0b0b7781e5f360e5c251cd2829a
Irrisam <tristanduchamp@hotmail.fr>
2023-06-13 13:37:56 +0200

    add task 00

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A module with as simple empty Square class"""
+
+
+def add_integer(a, b=98):
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    elif type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    elif type(a) is float:
+        a = int(a)
+    elif type(b) is float:
+        b = int(b)
+    else:
+        return a + b

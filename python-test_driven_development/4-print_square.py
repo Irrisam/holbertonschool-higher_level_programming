18df9481cc131b3fcf9aafa92352aa4a08085af7
Irrisam <tristanduchamp@hotmail.fr>
2023-06-15 14:04:21 +0200

    add task 05

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        if size != 0:
+            print("#" * size)
+        else:
+            print()
+
+
+print_square(-1.2)

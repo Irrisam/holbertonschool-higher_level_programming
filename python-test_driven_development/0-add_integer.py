5718952e6714d6907664ace712a8d076ef04ecde
Irrisam <tristanduchamp@hotmail.fr>
2023-06-15 08:11:57 +0200

    ad task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""A module with as simple empty Square class"""
+"""Module that adds 2 integers and return the sum
+    Args:
+        a (int):  integers or floats
+        b (int):  integers or floats
+"""
 
 
 def add_integer(a, b=98):
-    if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
-    elif type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
-    elif type(a) is float:
+    """Function that add two integers or float
+    and return the sum.
+    """
+    try:
+        if type(a) is not int and type(a) is not float:
+            raise TypeError("a must be an integer")
+        if type(b) is not int and type(b) is not float:
+            raise TypeError("b must be an integer")
         a = int(a)
-    elif type(b) is float:
         b = int(b)
-    else:
         return a + b
+    except Exception as e:
+        return e

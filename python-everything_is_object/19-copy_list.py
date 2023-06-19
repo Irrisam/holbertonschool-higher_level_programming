9ab53d996a3134ca4f6e26ad83cace8eedd1b363
Irrisam <tristanduchamp@hotmail.fr>
2023-06-19 21:22:25 +0200

    trying 19 chanegs ?

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,15 +1,3 @@
-#!/usr/bin/python3
-"""Module that copies a list"""
-
-
 def copy_list(a_list):
-    """copies a list
-
-    Args:
-        a_list (list): list to be copied
-
-    Returns:
-        list: copy of new list
-    """
     b = a_list[:]
     return b

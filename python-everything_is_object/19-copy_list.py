7b5854beaa80892eccd658aef47afd2ee65ed0a0
Irrisam <tristanduchamp@hotmail.fr>
2023-06-19 21:09:29 +0200

    correct 19 with requirements

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python3
+"""Module that copies a list"""
+
+
 def copy_list(a_list):
+    """copies a list
+
+    Args:
+        a_list (list): list to be copied
+
+    Returns:
+        list: copy of new list
+    """
     b = a_list[:]
     return b

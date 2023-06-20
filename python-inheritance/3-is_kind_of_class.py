255d99990b92c508b1a57a8a831ce852ffb4d3e0
Irrisam <tristanduchamp@hotmail.fr>
2023-06-20 08:59:13 +0200

    Add comments

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""checks if an object inherits from kind of the same class"""
+
+
 def is_kind_of_class(obj, a_class):
+    """checks if an object inherits from kind of the same class
+
+    Args:
+        obj (object): object to check
+        a_class (class): class to check from
+
+    Returns:
+        Bool: True if true, False if not
+    """
     if isinstance(obj, a_class):
         return True
     return False

7e80b05971f325410fd608e22904953ba5952d44
Irrisam <tristanduchamp@hotmail.fr>
2023-06-20 09:17:06 +0200

    add task 7

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""dunno what it does yet"""
+
+
+class BaseGeometry:
+    """who knows, I do not"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")

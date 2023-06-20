9db345e416dbbfedf2d815e9922b3600db8e0b2c
Irrisam <tristanduchamp@hotmail.fr>
2023-06-20 09:11:00 +0200

    add  task 5

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """check for inheritance
+
+    Args:
+        obj (class): check for inhritance to
+        a_class (class): check for inheritance from
+
+    Returns:
+        Bool: True is true, else, Flase
+    """
+    if type(obj) is not a_class:
+        return issubclass(type(obj), a_class)

6f2e7cd7ce18fa559b536ba159449be5f5348cdf
Irrisam <tristanduchamp@hotmail.fr>
2023-06-21 10:55:09 +0200

    removed main

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -25,24 +25,3 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be an integer".format(name))
-
-
-bg = BaseGeometry() 
-
-bg.integer_validator("my_int", 12)
-bg.integer_validator("width", 89)
-
-try:
-    bg.integer_validator("name", "John")
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    bg.integer_validator("age", 0)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    bg.integer_validator("distance", -4)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))

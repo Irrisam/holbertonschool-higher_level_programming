d685cce3fe49157521624e40461e1c3ceaa0b739
Irrisam <tristanduchamp@hotmail.fr>
2023-06-21 10:52:35 +0200

    corrected modifications

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -9,19 +9,3 @@ class BaseGeometry:
 
     def area(self):
         raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """valdiates value argument
-
-        Args:
-            name (string): name of value
-            value (int): int value of value
-
-        Raises:
-            TypeError: if value is not int 
-            ValueError: if value is egual or less than 0
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(self.name))
-        if value <= 0:
-            raise ValueError("{} must be an integer".format(self.name))
---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""dunno what it does yet"""
+
+
+class BaseGeometry:
+    """who knows, I do not"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """valdiates value argument
+
+        Args:
+            name (string): name of value
+            value (int): int value of value
+
+        Raises:
+            TypeError: if value is not int 
+            ValueError: if value is egual or less than 0
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(self.name))
+        if value <= 0:
+            raise ValueError("{} must be an integer".format(self.name))

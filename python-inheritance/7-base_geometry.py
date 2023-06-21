fe45495fadcb32e1bc283be730aba911c8e02fe6
Irrisam <tristanduchamp@hotmail.fr>
2023-06-21 10:54:56 +0200

    corrected self statement

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -22,6 +22,27 @@ class BaseGeometry:
             ValueError: if value is egual or less than 0
         """
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(self.name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be an integer".format(self.name))
+            raise ValueError("{} must be an integer".format(name))
+
+
+bg = BaseGeometry() 
+
+bg.integer_validator("my_int", 12)
+bg.integer_validator("width", 89)
+
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))

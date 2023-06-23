1bd7ab2d195800091c5f609b8493a1c5f4fa7a79
Irrisam <tristanduchamp@hotmail.fr>
2023-06-23 16:58:10 +0200

    readd pro

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -35,3 +35,7 @@ class BaseGeometry():
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+
+test = BaseGeometry(1)
+integer

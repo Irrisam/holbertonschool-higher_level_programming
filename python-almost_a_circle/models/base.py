8ba4abef979f9a2e20a4df708c1035b6ffcbe425
Irrisam <tristanduchamp@hotmail.fr>
2023-06-27 10:50:53 +0200

    ass task 01

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,15 +8,79 @@ class Base:
     """
     nb_objects = 0
 
-    def __init__(self, id=None):
+    def __init__(self, id=None, width=0, height=0, x=0, y=0):
         """initialized instances
 
         Args:
             id (int,optional): tracks id of different shapes. Defaults to None.
         """
         self.__id = id
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
         if id is not None:
             self.id = id
         else:
             Base.nb_objects += 1
             self.id = Base.nb_objects
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        self.width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.height = value
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.x
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        self.x = value
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.y
+
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        self.y = value
+
+
+class Rectangle(Base):
+    """rectangle class
+
+    Args:
+        Base (class): parent class
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initializes instances of rectangles
+
+        Args:
+            width (int): rectangle width
+            height (int): rectangle height
+            x (int, optional): x axis. Defaults to 0.
+            y (int, optional): y axis. Defaults to 0.
+            id (int, optional): shape's id. Defaults to None.
+        """
+        super().__init__(id)

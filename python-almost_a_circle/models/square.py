add61fbb26e24c2de9bd76c72b5af27ed8d0ab5b
Irrisam <tristanduchamp@hotmail.fr>
2023-06-29 14:15:30 +0200

    add task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,17 +5,34 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """square, defines a sub class of rectangle class
+
+    Args:
+        Rectangle (parent class): parent class used to derive for a square
+    """
     def __init__(self, size, x=0, y=0, id=None):
+        """initialization of square instances 
+
+        Args:
+            size (int): size of square
+            x (int, optional): x axis value. Defaults to 0.
+            y (int, optional): y axis value. Defaults to 0.
+            id (int, optional): id of shape. Defaults to None.
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """rewrites str to return approriate values
+
+        Returns:
+            string: values of square
+        """
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x, self.y, self.width)
 
     @property
     def size(self):
         """property size eguals to width or height
-
         Returns:
             int: defines square size
         """

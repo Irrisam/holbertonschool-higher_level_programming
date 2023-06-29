f360586325d6df6b091ebb81d835d2b5cfc92702
Irrisam <tristanduchamp@hotmail.fr>
2023-06-29 15:21:57 +0200

    add comments

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,7 +23,17 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """returns json value of object in json
+
+        Args:
+            list_dictionaries (list): returns string repr of shapes
+
+        Returns:
+            string: repr of object in json
+        """
         if list_dictionaries is None or []:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+        
+        

e66559fb87d35289161002e3267735ac14db6730
Irrisam <tristanduchamp@hotmail.fr>
2023-06-29 10:37:38 +0200

    add task 4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,11 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """returns area
+
+        Returns:
+            int: square area of rectangle
+        """
+        return self.width * self.height

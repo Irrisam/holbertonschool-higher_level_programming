b1b330d10d5a2c13c34e971e5109e6daa94ce0de
Irrisam <tristanduchamp@hotmail.fr>
2023-06-29 11:51:02 +0200

    add task 8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -108,7 +108,11 @@ class Rectangle(Base):
 
     def display(self):
         """displays the rectangle"""
+        for k in range(self.x):
+            print()
         for i in range(self.height):
+            for g in range(self.y):
+                print(" ", end="")
             for j in range(self.width):
                 print("#", end="")
             j = 0

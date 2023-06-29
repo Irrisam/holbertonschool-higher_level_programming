b40db45562cb81c46e717f87685769e152040c11
Irrisam <tristanduchamp@hotmail.fr>
2023-06-29 15:19:24 +0200

    add task 16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """shebangs indeed"""
 
+import json
 
 class Base:
     """Base class of program
@@ -20,3 +21,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)

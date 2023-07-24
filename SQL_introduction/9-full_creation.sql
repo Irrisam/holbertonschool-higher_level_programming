05e0456b20075d33e31c4461ddf02442d74b5799
Irrisam <tristanduchamp@hotmail.fr>
2023-07-24 15:58:41 +0200

    add task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,6 @@
 -- create new table with rows 
-CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256), score int),
+CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256), score int);
+
 INSERT INTO second_table VALUES (
     (1,"John", 10),
     (2, "Alex", 3),

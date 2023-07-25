bfdba9e0a16e06cab4bc6c3a8e99c9db514c0c73
Irrisam <tristanduchamp@hotmail.fr>
2023-07-25 13:52:32 +0200

    rectify an extra s

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create a new user 
 CREATE USER IF NOT EXISTS "user_0d_1"@"local_host" IDENTIFIED BY "user_0d_1_pwd";
-GRANTS ALL PRIVILEGES ON "." to "user_0d_1"@"local_host";
+GRANT ALL PRIVILEGES ON "." TO "user_0d_1"@"local_host";

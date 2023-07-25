9690615f95bb6269f3c472c666575e2693e2319b
Irrisam <tristanduchamp@hotmail.fr>
2023-07-25 13:43:53 +0200

    added more quotes to split values

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- shwo privileges of users 
-SHOW GRANTS FOR "user_0d_1@local_host";
-SHOW GRANTS FOR "user_0d_2@local_host";
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

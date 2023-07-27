58228a1a91c15db7b8824943f808a3f1e759ff1e
Irrisam <tristanduchamp@hotmail.fr>
2023-07-27 09:43:33 +0200

    add task 2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- create database and user 
+CREATE DATABASE IF NOT EXISTS htbn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'local_host' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON '.' TO 'user_0d_2'@'local_host' IDENTIFIED BY 'user_0d_2_pwd';
+ 

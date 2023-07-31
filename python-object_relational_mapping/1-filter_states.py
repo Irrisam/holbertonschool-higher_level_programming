7080cb290084bf61009234f6cc1f443dcaf2ea5e
Irrisam <tristanduchamp@hotmail.fr>
2023-08-01 01:31:16 +0200

    OKAY THATS WORKS ON LOCAL

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" a script that lists all states with a name starting with N"""
+"""a script that lists all states with a name starting with N"""
 
 import MySQLdb
 import sys
@@ -13,10 +13,12 @@ if __name__ == "__main__":
         db=sys.argv[3]
         )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
-        if row[1[0]] == "N":
+        if row[1].startswith("N"):
             print(row)
         else:
             pass
+    cur.close()
+    db.close()

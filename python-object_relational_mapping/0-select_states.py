2f8492cd07b06ac724c3841d39612ff1a3e370ad
Irrisam <tristanduchamp@hotmail.fr>
2023-07-31 18:39:03 +0200

    add import locker

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,15 +3,16 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-cur = db.cursor()
+    cur = db.cursor()
 
 
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(f"{row}")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(f"{row}")
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()

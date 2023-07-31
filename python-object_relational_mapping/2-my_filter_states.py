76373b389b77a941b95d606616daf1c080137db6
Irrisam <tristanduchamp@hotmail.fr>
2023-08-01 01:47:47 +0200

    add task2, working on local(wrong repo push)

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a script that lists all states with a name starting with N"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    search_value = sys.argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == search_value:
+            print(row)
+        else:
+            pass
+    cur.close()
+    db.close()

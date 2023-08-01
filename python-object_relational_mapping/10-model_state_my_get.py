f17fabd892b0fdb1c022a16fd41b7a9c783d5393
Irrisam <tristanduchamp@hotmail.fr>
2023-08-02 00:17:52 +0200

    corrected comments on 7 & 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -8,23 +8,25 @@ import sys
 
 
 if __name__ == "__main__":
-    # Create an instance of SQLAlchemy engine
+    """Create an instance of SQLAlchemy engine"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:\
                             3306/{}'.format(sys.argv[1],
                                             sys.argv[2],
                                             sys.argv[3]))
 
-    # Create a session to interact with the database
+    """Create a session to interact with the database"""
     session = Session(engine)
-    # Query all State objects
+    """Query all State objects"""
     query = sys.argv[4]
     search_name = query
     try :
         states = session.query(State).filter(State.name == search_name).order_by(State.id).all()
         if len(states) == 0:
             print("Not Found")
+        elif states is None:
+            print("Nothing")
         else:
-            # Print the id and name of each State object
+            """Print the id and name of each State object"""
             for state in states:
                 print(state.id)
     except: 
--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all State objects from the database hbtn_0e_6_usa"""
+"""Lists all State objects from the database hbtn_0e_6_usa."""
 
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -8,17 +8,17 @@ import sys
 
 
 if __name__ == "__main__":
-    # Create an instance of SQLAlchemy engine
+    """Create an instance of SQLAlchemy engine"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:\
                             3306/{}'.format(sys.argv[1],
                                             sys.argv[2],
                                             sys.argv[3]))
 
-    # Create a session to interact with the database
+    """Create a session to interact with the database"""
     session = Session(engine)
-    # Query all State objects
+    """Query all State objects"""
     states = session.query(State).order_by(State.id).all()
 
-    # Print the id and name of each State object
+    """Print the id and name of each State object"""
     for state in states:
         print(f"{state.id}: {state.name}")

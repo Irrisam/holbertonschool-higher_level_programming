c37b8ffd1034dca4af608cc8b5ec4ec8576446db
Irrisam <tristanduchamp@hotmail.fr>
2023-08-01 19:00:02 +0200

    add model state

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class definition of a State and an instance Base = declarative_base():"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ """
+    __tablename__ = "states"
+    """ primary key gives unique attributes non null and autogened"""
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)

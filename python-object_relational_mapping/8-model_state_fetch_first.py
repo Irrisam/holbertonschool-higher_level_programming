f63a63ba92f5a163f7f17a4be5cdd8fa2c14544e
Irrisam <tristanduchamp@hotmail.fr>
2023-08-02 00:12:42 +0200

    switched comments style

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,20 +8,19 @@ import sys
 
 
 if __name__ == "__main__":
-    # Create an instance of SQLAlchemy engine
+    """Create an instance of SQLAlchemy engine"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:\
                             3306/{}'.format(sys.argv[1],
                                             sys.argv[2],
                                             sys.argv[3]))
 
-    # Create a session to interact with the database
+    """Create a session to interact with the database"""
     session = Session(engine)
-    # Query all State objects
+    """Query all State objects"""
     states = session.query(State).order_by(State.id).first()
 
     if states is None:
         print("Nothing")
-    # Print the id and name of each State object
     else:
         print(f"{states.id}: {states.name}")
 

cd1f55e736ed1676bb185544ce3a05095617d9d3
Irrisam <tristanduchamp@hotmail.fr>
2023-08-01 21:53:32 +0200

    update task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,33 +1,27 @@
-#!usr/bin/python
-"""a script that lists all State objects from the database"""
-# Import the necessary modules
-import MySQLdb
+#!/usr/bin/python3
+"""A script that prints the first State object
+from the database hbtn_0e_6_usa"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
 from model_state import Base, State
-# Importing the Base and Stateclasses from model_state
-if __name__ == "__main__":
-    # Create a MySQL engine using SQLAlchemy
-    # The script takes the MySQL username, password,
-    # and database name as command-line arguments
-    # and uses them to create the connection URL for the engine
-    engine = create_engine(
-         """mysql://{}:{}@localhost:3306/{}
-         """.format(sys.argv[1], sys.argv[2], sys.argv[3])
-    )
 
-    # Create a session to interact with the database
+if __name__ == "__main__":
+    """Create an engine"""
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    """Create a session"""
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query all State objects from the database and sort them by states.id
-    # in ascending order
-    states = session.query(State).order_by(State.id).all()
+    """Query the database and format output"""
+    state = session.query(State).first()
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
 
-    # Print the results, displaying the state id and name
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-    # Close the session
+    """Close the session"""
     session.close()

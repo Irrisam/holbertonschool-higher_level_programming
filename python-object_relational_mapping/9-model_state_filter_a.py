273c8f35906320cfcdd47a26ec583469bd90c8b9
Irrisam <tristanduchamp@hotmail.fr>
2023-08-02 00:07:45 +0200

    trying ben's file

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Lists all State objects from the database hbtn_0e_6_usa"""
+"""Lists all State objects that contain the
+letter a from the database hbtn_0e_6_usa"""
 
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -8,20 +9,20 @@ import sys
 
 
 if __name__ == "__main__":
-    # Create an instance of SQLAlchemy engine
+    """Create an instance of SQLAlchemy engine"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:\
                             3306/{}'.format(sys.argv[1],
                                             sys.argv[2],
                                             sys.argv[3]))
 
-    # Create a session to interact with the database
+    """Create a session to interact with the database and Query"""
     session = Session(engine)
-    # Query all State objects
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    """filter() with like("%a%")"""
+    states = session.query(State).filter(State.name.like("%a%"))\
+        .order_by(State.id).all()
 
-    # Print the id and name of each State object
     if states is None:
         print("Nothing")
     else:
         for state in states:
-            print(state.name)
+            print(f"{state.id}: {state.name}")

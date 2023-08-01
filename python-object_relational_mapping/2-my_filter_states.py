5a6bf406a27caf6bbc4b3c24070bf8162580d9b2
Irrisam <tristanduchamp@hotmail.fr>
2023-08-01 12:05:18 +0200

    rety vlad file ?

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a script that lists all states with a name starting with N"""
+"""A script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument"""
 
 import MySQLdb
 import sys
@@ -11,14 +12,21 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
-        )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id")
-    rows = cur.fetchall()
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+        """
+        SELECT *
+        FROM states
+        WHERE states.name LIKE BINARY '{}'
+        ORDER BY states.id
+        """.format(sys.argv[4])
+    )
+
+    rows = cursor.fetchall()
     for row in rows:
-        if row[1].startswith("N"):
-            print(row)
-        else:
-            pass
-    cur.close()
+        print(row)
+
+    cursor.close()
     db.close()

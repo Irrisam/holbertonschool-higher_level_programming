94601d35f54d7f481b87f57aa8c976f0bb95e638
Irrisam <tristanduchamp@hotmail.fr>
2023-08-01 15:26:21 +0200

    try to remove parametarized variable

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -14,13 +14,15 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    query = """
-    SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id
-    """
-    cur.execute(query)
+
+    cur.execute(
+        """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id
+        """
+    )
 
     rows = cur.fetchall()
     for row in rows:

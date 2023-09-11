ccb6f5897ce4bb449f14d62b937dc2f90b9e3ff8
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-09-11 10:22:15 +0200

    add task 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,7 @@
+var red_header = document.getElementById("red_header");
+red_header.addEventListener("click", red_header_red);
+var header = document.querySelector("header");
+
+function red_header_red(){
+    header.style.color = "red";
+}

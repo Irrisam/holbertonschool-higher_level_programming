08982a77f8519eab5055624fb5cd70f04a12c61c
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-09-12 13:59:39 +0200

    add task 5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,6 @@
+headr = document.querySelector("header");
+update_header = document.getElementById("update_header");
+
+update_header.addEventListener("click", function() {
+headr.innerText = "New Header !!!";
+})
